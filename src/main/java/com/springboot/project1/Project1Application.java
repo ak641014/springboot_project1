e4f014d4b1526eb19eded5922a3685d8979ec36a
Akanksha Sharma <akanksha641014@gmail.com>
2025-03-29 10:23:19 +0530

    Root Update

--- src/main/java/com/springboot/project1/Project1Application.java
+++ src/main/java/com/springboot/project1/Project1Application.java
@@ -2,12 +2,22 @@ package com.springboot.project1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Project1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
 	}
-
+	@GetMapping("/")
+	public String homepage(){
+		return "Homepage";
+	}
+	@GetMapping("/admin")
+	public String admin(){
+		return "Admin";
+	}
 }
